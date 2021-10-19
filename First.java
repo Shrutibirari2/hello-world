b9613e5e24c435fc699bb6dc5e5060bc7754288e
Shruti birari <90673369+Shrutibirari2@users.noreply.github.com>
2021-10-19 18:42:49 +0530

    initial commit

---
+++ First.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+class First
+{
+         public static void main(String args[])
+            { 
+                   String name;
+                   Scanner sc = new Scanner(System.in);
+                   System.out.println("May i know your name");
+                   name = sc.nextLine();
+                   System.out.println("Welcome " +name);
+             }
+}
+                   
